55cea0ede98e3ddf93ee00626c7c9cde3a136d78
yanivdg <134487944+yanivdg@users.noreply.github.com>
2023-10-06 20:10:55 +0300

    Update index.js
    changing  script

--- static/js/index.js
+++ static/js/index.js
@@ -1,34 +1,30 @@
-// index.js
-// Function to send a POST request and get the images
-async function getImages() {
-const response = await fetch('https://vokhppyw7l.execute-api.us-west-1.amazonaws.com/default', {
-    method: 'GET',
-    mode: 'cors', // add this line
-    headers: {
-        'Content-Type': 'application/json'
-    },
-});
-const imagebase = await response.json();
-return imagebase;
-}
-/*
-// Function to create an img element from a base64-encoded image
-function createImageElement(imagedata) {
-    var img = document.createElement('img');
-    //img.src = 'data:image/jpeg;base64,' + base64Image;
-    img.src =  imagedata;
-    return img;
-}
-
-// Function to display the images on the page
-function displayImages(imagedata) {
-    var container = document.getElementById('image-container');
-    for (var i = 0; i < imagedata.length; i++) {
-        var img = createImageElement(imagedata[i]);
-        container.appendChild(img);
+fetch('URL_OF_YOUR_AWS_SERVICE')
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error('Request failed');
     }
+    return response.json();
+  })
+  .then((data) => {
+    // Handle the JSON response data here
+    // Example: Assuming 'photos' is an array in the response
+    //displayPhotos(data.photos); // Implement displayPhotos function
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
+/*
+function displayPhotos(photos) {
+  const photoContainer = document.getElementById('photo-container');
+
+  // Loop through the photos and create HTML elements for each photo
+  photos.forEach((photo) => {
+    const imgElement = document.createElement('img');
+    imgElement.src = photo.imageUrl; // Set the image URL from the JSON data
+
+    // Add the image to the photo container
+    photoContainer.appendChild(imgElement);
+  });
 }
 */
-// Use the functions
-getImages();//.then(displayImages);
 
