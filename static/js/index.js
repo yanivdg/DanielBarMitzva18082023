0f30100e9927bb3eed573a08235fe052897d70dc
yanivdg <134487944+yanivdg@users.noreply.github.com>
2023-10-06 22:11:54 +0300

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,30 +1,49 @@
-fetch('https://vokhppyw7l.execute-api.us-west-1.amazonaws.com/default')
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error('Request failed');
-    }
-    return response.json();
-  })
-  .then((data) => {
-    // Handle the JSON response data here
-    // Example: Assuming 'photos' is an array in the response
-    //displayPhotos(data.photos); // Implement displayPhotos function
-  })
-  .catch((error) => {
-    console.error('Error:', error);
-  });
-/*
-function displayPhotos(photos) {
-  const photoContainer = document.getElementById('photo-container');
+const https = require('https');
 
-  // Loop through the photos and create HTML elements for each photo
-  photos.forEach((photo) => {
-    const imgElement = document.createElement('img');
-    imgElement.src = photo.imageUrl; // Set the image URL from the JSON data
+// Initial request to the AWS service
+const awsUrl = 'https://vokhppyw7l.execute-api.us-west-1.amazonaws.com/default';
 
-    // Add the image to the photo container
-    photoContainer.appendChild(imgElement);
-  });
-}
-*/
+const initialRequest = https.request(awsUrl, (response) => {
+  if (response.statusCode === 302 && response.headers.location) {
+    // Handle the redirection
+    const redirectedUrl = response.headers.location;
 
+    // Make a new request to the redirected URL
+    const redirectedRequest = https.request(redirectedUrl, (redirectedResponse) => {
+      let responseData = '';
+
+      redirectedResponse.on('data', (chunk) => {
+        responseData += chunk;
+      });
+
+      redirectedResponse.on('end', () => {
+        // Handle the response from the redirected URL as needed
+        console.log('Response from Redirected URL:', responseData);
+      });
+    });
+
+    redirectedRequest.on('error', (error) => {
+      console.error('Error making request to redirected URL:', error);
+    });
+
+    redirectedRequest.end();
+  } else {
+    // Handle other types of responses (e.g., JSON, HTML) from the AWS service as needed
+    let responseData = '';
+
+    response.on('data', (chunk) => {
+      responseData += chunk;
+    });
+
+    response.on('end', () => {
+      // Handle the initial response from the AWS service as needed
+      console.log('Response from AWS service:', responseData);
+    });
+  }
+});
+
+initialRequest.on('error', (error) => {
+  console.error('Error making initial request to AWS service:', error);
+});
+
+initialRequest.end();
