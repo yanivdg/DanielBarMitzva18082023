826159f55a8f26724097230a004d5f98e159db18
yanivdg <134487944+yanivdg@users.noreply.github.com>
2023-10-06 22:25:51 +0300

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,49 +1,61 @@
-const https = require('https');
-
 // Initial request to the AWS service
 const awsUrl = 'https://vokhppyw7l.execute-api.us-west-1.amazonaws.com/default';
 
-const initialRequest = https.request(awsUrl, (response) => {
-  if (response.statusCode === 302 && response.headers.location) {
-    // Handle the redirection
-    const redirectedUrl = response.headers.location;
+fetch(awsUrl)
+  .then((response) => {
+    if (response.status === 302) {
+      // Handle the redirection
+      const redirectedUrl = response.headers.get('Location');
 
-    // Make a new request to the redirected URL
-    const redirectedRequest = https.request(redirectedUrl, (redirectedResponse) => {
-      let responseData = '';
-
-      redirectedResponse.on('data', (chunk) => {
-        responseData += chunk;
+      // Make a new request to the redirected URL
+      fetch(redirectedUrl)
+        .then((redirectedResponse) => redirectedResponse.text())
+        .then((responseData) => {
+          // Handle the response from the redirected URL as needed
+          console.log('Response from Redirected URL:', responseData);
+        })
+        .catch((error) => {
+          console.error('Error making request to redirected URL:', error);
+        });
+    } else {
+      // Handle other types of responses (e.g., JSON, HTML) from the AWS service as needed
+      return response.text().then((responseData) => {
+        // Handle the initial response from the AWS service as needed
+        console.log('Response from AWS service:', responseData);
       });
+    }
+  })
+  .catch((error) => {
+    console.error('Error making initial request to AWS service:', error);
+  });
+// Initial request to the AWS service
+const awsUrl = 'YOUR_AWS_URL_HERE';
 
-      redirectedResponse.on('end', () => {
-        // Handle the response from the redirected URL as needed
-        console.log('Response from Redirected URL:', responseData);
+fetch(awsUrl)
+  .then((response) => {
+    if (response.status === 302) {
+      // Handle the redirection
+      const redirectedUrl = response.headers.get('Location');
+
+      // Make a new request to the redirected URL
+      fetch(redirectedUrl)
+        .then((redirectedResponse) => redirectedResponse.text())
+        .then((responseData) => {
+          // Handle the response from the redirected URL as needed
+          console.log('Response from Redirected URL:', responseData);
+        })
+        .catch((error) => {
+          console.error('Error making request to redirected URL:', error);
+        });
+    } else {
+      // Handle other types of responses (e.g., JSON, HTML) from the AWS service as needed
+      return response.text().then((responseData) => {
+        // Handle the initial response from the AWS service as needed
+        console.log('Response from AWS service:', responseData);
       });
-    });
+    }
+  })
+  .catch((error) => {
+    console.error('Error making initial request to AWS service:', error);
+  });
 
-    redirectedRequest.on('error', (error) => {
-      console.error('Error making request to redirected URL:', error);
-    });
-
-    redirectedRequest.end();
-  } else {
-    // Handle other types of responses (e.g., JSON, HTML) from the AWS service as needed
-    let responseData = '';
-
-    response.on('data', (chunk) => {
-      responseData += chunk;
-    });
-
-    response.on('end', () => {
-      // Handle the initial response from the AWS service as needed
-      console.log('Response from AWS service:', responseData);
-    });
-  }
-});
-
-initialRequest.on('error', (error) => {
-  console.error('Error making initial request to AWS service:', error);
-});
-
-initialRequest.end();
