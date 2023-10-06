6401444c9bae1fe964eca9360493a8a0e6518539
yanivdg <134487944+yanivdg@users.noreply.github.com>
2023-10-06 11:18:41 +0300

    Update index.js
    fixing fetch request to get

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,7 @@
 // index.js
 // Function to send a POST request and get the images
 async function getImages() {
+    /*
 const response = await fetch('https://vokhppyw7l.execute-api.us-west-1.amazonaws.com/default', {
     method: 'GET',
     mode: 'cors', // add this line
@@ -10,6 +11,27 @@ const response = await fetch('https://vokhppyw7l.execute-api.us-west-1.amazonaws
 });
 const imagesBase64 = await response.json();
 return imagesBase64;
+*/
+  try {
+    // Define the URL you want to make a GET request to.
+    const url = 'https://vokhppyw7l.execute-api.us-west-1.amazonaws.com/default'; // Replace with your API endpoint
+
+    // Make the GET request using fetch and await the response.
+    const response = await fetch(url);
+
+    // Check if the response status is OK (status code 200).
+    if (!response.ok) {
+      throw new Error('Request failed');
+    }
+    // Parse the response as JSON.
+    const data = await response.json();
+    // Handle the response data here.
+    console.log('Response:', data);
+    return data
+  } catch (error) {
+    // Handle errors here.
+    console.error('Error:', error);
+  }
 }
 // Function to create an img element from a base64-encoded image
 function createImageElement(base64Image) {
