c109bc327cb121f9de6d737a1c8e0a4812abea07
yanivdg <134487944+yanivdg@users.noreply.github.com>
2023-10-07 21:46:22 +0300

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,13 +1,36 @@
-fetch('https://vokhppyw7l.execute-api.us-west-1.amazonaws.com/default', {
-  method: 'GET',
-  headers: {
+
+function makeCORSRequest() {
+  const apiUrl = 'https://vokhppyw7l.execute-api.us-west-1.amazonaws.com/default';
+  const headers = {
+    'Content-Type': 'application/json', // Specify the content type if needed
     'X-Api-Caller': 'javascript', // Use a custom header, e.g., 'X-Api-Caller'
     'Access-Control-Allow-Origin':'*'
-  },
-})
-  .then((response) => {
-    console.log(response);
+  };
+
+  // Perform a GET request with custom headers
+  return fetch(url, {
+    method: 'GET',
+    headers: headers,
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Request failed');
+      }
+      return response.json(); // Assuming the response is in JSON format
+    })
+    .then((data) => {
+      // Handle the response data here
+      return data;
+    })
+    .catch((error) => {
+      // Handle any errors that occurred during the request
+      throw error;
+    });
+}
+makeCORSRequest()
+  .then((data) => {
+    console.log('Response Data:', data);
   })
   .catch((error) => {
-    console.log("error");
+    console.error('Error:', error);
   });
