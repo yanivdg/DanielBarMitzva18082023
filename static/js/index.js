15d607074ab119967a61d5cea13fb799bc08457e
yanivdg <134487944+yanivdg@users.noreply.github.com>
2023-10-07 22:35:41 +0300

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,13 +1,14 @@
 // This is an example of using the async/await pattern within an async function
 
 async function fetchData() {
-  const apiUrl = 'YOUR_API_ENDPOINT';
-  const customHeaders = {
-    'x-api-caller': 'specific-party',
+  const apiUrl = 'https://vokhppyw7l.execute-api.us-west-1.amazonaws.com/default';
+  const headers = {
+    'Content-Type': 'application/json', // Specify the content type if needed
+    'X-Api-Caller': 'javascript', // Use a custom header, e.g., 'X-Api-Caller'
   };
 
   try {
-    const data = await makeCORSRequest(apiUrl, customHeaders);
+    const data = await makeCORSRequest(apiUrl, headers);
     console.log('Response Data:', data);
   } catch (error) {
     console.error('Error:', error);
@@ -16,3 +17,30 @@ async function fetchData() {
 
 // Call the fetchData function to start the async operation
 fetchData();
+
+async function makeCORSRequest(Url, headers) {
+  try {
+    // Perform a GET request with custom headers
+    const response = await fetch(Url, {
+      method: 'GET',
+      headers: headers,
+    });
+
+    if (!response.ok) {
+      throw new Error('Request failed');
+    }
+
+    // Parse and return the response data
+    return await response.json(); // Assuming the response is in JSON format
+  } catch (error) {
+    // Handle any errors that occurred during the request
+    throw error;
+  }
+}
+
+try {
+  const data = await makeCORSRequest();
+  console.log('Response Data:', data);
+} catch (error) {
+  console.error('Error:', error);
+}
