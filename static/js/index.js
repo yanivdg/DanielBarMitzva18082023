d3e5a6c10bf359a9264e487948dbfae814d33b36
yanivdg <134487944+yanivdg@users.noreply.github.com>
2023-10-07 13:12:14 +0300

    Update index.js
    changing js request

--- static/js/index.js
+++ static/js/index.js
@@ -1,30 +1,12 @@
-// Initial request to the AWS service
-const awsUrl = 'https://vokhppyw7l.execute-api.us-west-1.amazonaws.com/default';
-
-fetch(awsUrl)
+fetch('https://vokhppyw7l.execute-api.us-west-1.amazonaws.com/default', {
+  method: 'GET',
+  headers: {
+    'X-Api-Caller': 'javascript', // Use a custom header, e.g., 'X-Api-Caller'
+  },
+})
   .then((response) => {
-    if (response.status === 302) {
-      // Handle the redirection
-      const redirectedUrl = response.headers.get('Location');
-
-      // Make a new request to the redirected URL
-      fetch(redirectedUrl)
-        .then((redirectedResponse) => redirectedResponse.text())
-        .then((responseData) => {
-          // Handle the response from the redirected URL as needed
-          console.log('Response from Redirected URL:', responseData);
-        })
-        .catch((error) => {
-          console.error('Error making request to redirected URL:', error);
-        });
-    } else {
-      // Handle other types of responses (e.g., JSON, HTML) from the AWS service as needed
-      return response.text().then((responseData) => {
-        // Handle the initial response from the AWS service as needed
-        console.log('Response from AWS service:', responseData);
-      });
-    }
+    console.log(response);
   })
   .catch((error) => {
-    console.error('Error making initial request to AWS service:', error);
+    console.log("error");
   });
