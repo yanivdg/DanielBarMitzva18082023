566e4724dd0476eb30a5bd5de38e2f68994670e3
yanivdg <134487944+yanivdg@users.noreply.github.com>
2023-10-15 21:03:04 +0300

    Update index.js
    fixed from chatgpt check

--- static/js/index.js
+++ static/js/index.js
@@ -1,30 +1,31 @@
 // index.js
 // Function to send a POST request and get the images
 async function getRedirectOrImages(Url) {
-    // Make an HTTP GET request to the Python service with headers
-    const response = await fetch(Url, {
-    method: 'GET',
+    try {
+        // Make an HTTP GET request to the Python service with headers
+        const response = await fetch(Url, {
+            method: 'GET',
             headers: {
-            'Content-Type': 'application/json',
-            'X-Api-Caller': 'DanielBarMitzva18082023'
+                'Content-Type': 'application/json',
+                'X-Api-Caller': 'DanielBarMitzva18082023'
             }
-})
-    .then(response => response.text()) // Assuming the service returns plain text
-    .then(url => {
-        // Use the URL from the service response to change the browser's location
-        window.location.href = url;
-    })
-    .then(response => response.json())
-    .then(data => {
-                  // The response was a JSON object
-                  // Process your data as a JavaScript object
-                     const jsonresp = await response.json();
-                    console.log(jsonresp.body);
-                    return jsonresp;
-                })
-    .catch(error => {
+        });
+
+        if (response.headers.get('Content-Type').includes('application/json')) {
+            // JSON response
+            const data = await response.json();
+            // Process your data as a JavaScript object
+            console.log(data.body);
+            return data;
+        } else {
+            // Non-JSON response, treat it as a URL
+            const url = await response.text();
+            // Use the URL from the service response to change the browser's location
+            window.location.href = url;
+        }
+    } catch (error) {
         console.error('Error:', error);
-    });
+    }
 }
 
 // Use the functions
