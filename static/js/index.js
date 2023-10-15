9a0b72ef24e004054a884612448e424cf7ccfaa8
yanivdg <134487944+yanivdg@users.noreply.github.com>
2023-10-16 01:13:39 +0300

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -34,7 +34,7 @@ if (window.location.href.indexOf("code") != -1)
 {
     const encodedString = encodeURIComponent(window.location.href);
     // Construct the URL with the encoded string as a query parameter
-    url = url + "?code="+encodedString;
+    url = url + "?params="+encodedString;
 }
 getRedirectOrImages(url)
 
